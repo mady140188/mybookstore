5c3ab4ef042c0252427881ea50ec8c528bb16d1a
mkelapure@infocepts.com <mkelapure@infocepts.com>
2020-04-19 23:25:33 +0530

    Added bulk books add call

--- routes/books.js
+++ routes/books.js
@@ -45,9 +45,36 @@ router.post('/addbookmaster',function(req,res,next){
 			if(err){
 				res.status(400).send(err.detail);
 			}else{
-				res.status(200).send("Book added successfully");
+				res.status(201).send("Book added successfully");
 			}
 		});
+});
+
+/* POST bulk insert into bookmaster table*/
+router.post('/addbookmaster-bulk', function(req,res,next){
+	if(req.body == null || !Array.isArray(req.body)){
+		res.status(400).send("Invalid Request"); 
+	}
+	let book_author = "book_author";
+	let book_id = "book_id";
+	let book_name = "book_name";
+	let book_price = "book_price";
+	let queryStart = 'INSERT INTO all_books.book_master ('+book_author+', '+book_id+', '+book_name+', '+book_price+') VALUES '; 
+	let values = ""; 
+		for(let i=0;i<req.body.length;i++){
+		if(values !== ""){
+			values = values + ',';	
+		}	
+		values = values + '(\''+req.body[i][book_author]+'\', \''+req.body[i][book_id]+'\', \''+req.body[i][book_name]+'\', \''+req.body[i][book_price]+'\')';
+	}
+	let allQuery = queryStart+values+";";
+	client.query(allQuery, function(err, result){
+		if(err){
+			res.status(400).send(err.detail);
+		}else{
+			res.status(201).send("Bulk Books Added successfully");
+		}
+	});
 });
 
 module.exports = router;
