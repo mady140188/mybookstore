b08675ec900d500e24c7624496bef031375b8362
mkelapure@infocepts.com <mkelapure@infocepts.com>
2020-05-11 20:50:10 +0530

    Added API authentication with jwt token

--- app.js
+++ app.js
@@ -1,57 +1,104 @@
-//var createError = require('http-errors');
-var express = require('express');
 
+var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-
 var http = require('http');
+//API token
+const morgan = require('morgan'),
+jwt = require('jsonwebtoken'),
+bodyParser = require('body-parser'),
+config = require('./configurations/config');
+const ProtectedRoutes = express.Router();
+
 
 //Init express app and http server
 var app = express();
-
 var server = http.createServer(app);
-
 var port = '3000';
-
 app.set('port',port);
 
 
+//API token
+app.use(morgan('dev'));
+app.set('Secret', config.secret);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-//Import js files in routs folder
-//var indexRouter = require('./routes/index');
+
+//set up authentication
+app.post('/authenticate', function(req, res){
+
+  if(req.body.username === "mahendra"){
+    if(req.body.password === "123"){
+        const payload = {
+          check: true
+        };
+
+        var token = jwt.sign(payload, app.get('Secret'), {
+          expiresIn: 1440 //expires in 24 hours
+        });
+
+        res.json({
+          message: 'authentication done',
+          token: token
+        });
+    } else{
+      res.json({
+        message: "Please check your password!!!"
+      })
+    }
+  }else{
+    res.json({
+      message: "User not found!!!"
+    })
+  }
+
+});
+
+
+//Middleware routes
+app.use("/api", ProtectedRoutes);
+ProtectedRoutes.use(function(req, res, next){
+  //check header for token
+  var token  = req.headers['access-token'];
+
+  //decode token
+  if(token){
+    //verify secret and check if token has expired
+    jwt.verify(token, app.get('Secret'), function(err, decoded){
+      if(err){
+        return res.json({message:"Invalid token"});
+      }else{
+        //if every thing is good , save to request for other routes
+        req.decoded = decoded;
+        next();
+      }
+    })
+
+  }else{
+
+    //if no token
+    res.json({message: "No Token Provided!!!"})
+  }
+});
+
+ProtectedRoutes.get('/getproducts', function(req,res){
+
+  res.json({
+    "products": req.decoded
+  })
+
+});
+
+
+app.get('/', function(req, res){
+  res.send("Welcome to My-Book-Store");
+});
+
 var bookRouter = require('./routes/books');
-
-
-// view engine setup
-/*app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-*/
-
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/', indexRouter);
-
 app.use('/mybookstore', bookRouter);
 
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-  next(createError(404));
-});*/
-
-// error handler
-/*app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-*/
 server.listen(port, function(err){
 	if(err){
 		console.log(err);
@@ -60,4 +107,3 @@ server.listen(port, function(err){
 	console.log('Server listening on PORT:::'+port);
 });
 
-//module.exports = app;
